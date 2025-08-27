c08839a3869400a7af518b2d1ba833bb45a91950
devhenriquejs <luish7572@gmail.com>
2025-08-27 20:07:33 -0300

    Corrigindo a tarefa para converter de primitivo para wrapper (autoboxing)

--- src/com/conversaoCasting/Main.java
+++ src/com/conversaoCasting/Main.java
@@ -4,24 +4,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
-        // 1. Ler um valor do console
+
+        // 1. Ler um valor do console e armazenar em uma variável primitiva
         System.out.println("Digite um número inteiro: ");
         Scanner leitor = new Scanner(System.in);
-        
-        // Armazena o valor em uma variável de tipo primitivo (int)
-        int valorInt = leitor.nextInt();
-        
-        System.out.println("O valor digitado é: " + valorInt);
-        
-        // 2. Fazer o casting
-        // Aqui convertemos um 'int' para 'byte' (int é maior que byte)
-        // Isso exige um casting explícito.
-        byte valorByte = (byte) valorInt;
-        
-        // 3. Imprimir o resultado
-        System.out.println("O valor original (int) é: " + valorInt);
-        System.out.println("O valor convertido (byte) é: " + valorByte);
+
+        int valorPrimitivo = leitor.nextInt();
+
+        // 2. Fazer o autoboxing (conversão de primitivo para wrapper)
+        // Convertendo o tipo 'int' para o tipo 'Integer' (wrapper)
+        Integer valorWrapper = valorPrimitivo; // Autoboxing
+
+        // 3. Imprimir os resultados
+        System.out.println("O valor primitivo (int) é: " + valorPrimitivo);
+        System.out.println("O valor wrapper (Integer) é: " + valorWrapper);
 
         // Fechar o scanner
         leitor.close();
